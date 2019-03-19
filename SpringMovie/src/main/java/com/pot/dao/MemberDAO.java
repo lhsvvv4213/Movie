34458f6a86ec25742fdf3a8302c1eb0af3a44691
hansaem <hansaem@leehs>
2019-03-19 15:08:29 +0900

    ajax 완료

--- SpringMovie/src/main/java/com/pot/dao/MemberDAO.java
+++ SpringMovie/src/main/java/com/pot/dao/MemberDAO.java
@@ -11,22 +11,22 @@ import com.pot.dto.MemberVO;
 public interface MemberDAO {
 
 	// 회원리스트
-	@Select("select * from member")
+	@Select("select * from MEMBER")
 	public List<MemberVO> memberList();
 
 	// 중복아이디 체크	
-	@Select("select id from member where id=#{id}")
+	@Select("select id from MEMBER where id=#{id}")
 	public String idCheck(String id);
 
 	// 회원가입
-	@Insert("insert into member values(#{id},#{pw},#{name},#{email},#{phone},0,'guest')")
+	@Insert("insert into MEMBER values(#{id},#{pw},#{name},#{email},#{phone},0,'guest')")
 	public int memberInsert(MemberVO memberVO);
 	
-	@Insert("insert into login values(#{id},#{pw},1)")
+	@Insert("insert into LOGIN values(#{id},#{pw},1)")
 	public int loginInsert(Map<String, Object> map);
 	
 	// 로그인
-	@Select("select authority from login where id=#{id} and pw=#{pw}")
+	@Select("select authority from LOGIN where id=#{id} and pw=#{pw}")
 	public String login(Map<String, Object> map);
 
 }
--- SpringMovie/src/main/webapp/WEB-INF/views/admin/admin_Member.jsp
+++ SpringMovie/src/main/webapp/WEB-INF/views/admin/admin_Member.jsp
@@ -9,7 +9,7 @@
                     <div class="card mb-3">
                         <div class="card-body">
                             <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <table class="table table-bordered" id="dataTable" align="center" width="100%" cellspacing="0">
                                     <thead>
                                         <tr>
                                             <th>이름</th>
